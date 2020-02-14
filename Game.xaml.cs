eb071a938a4455342590f76ac80c71473edf7f3c
Kamil Rominski <kamil.rominski@gmail.com>
2020-02-14 21:37:38 +0100

    Added move ability to player; Scores for player and CPU are implemented. TODO: smooth move, ball self-moving, AI for CPU

--- Game.xaml.cs
+++ Game.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
 
 namespace Pong_csharp
 {
@@ -20,9 +22,33 @@ namespace Pong_csharp
     /// </summary>
     public partial class Game : Page
     {
+        int speed = 5;
+        int ballx = 5;
+        int bally = 5;
+        int score = 0;
+        int cpuscore = 0;
         public Game()
         {
             InitializeComponent();
+            /*DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(20);
+            timer.Start();*/
+        }
+        
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Down && Canvas.GetTop(Player) + Player.Height < 720)
+            {
+                //Object goes down
+                Canvas.SetTop(Player, Canvas.GetTop(Player) + 3);
+            }
+            if(e.Key == Key.Up && Canvas.GetTop(Player) > 0)
+            {
+                //Object goes up
+                Canvas.SetTop(Player, Canvas.GetTop(Player) - 3);
+            }
+            if (e.Key == Key.Escape)
+                System.Windows.Application.Current.Shutdown();
         }
     }
 }
